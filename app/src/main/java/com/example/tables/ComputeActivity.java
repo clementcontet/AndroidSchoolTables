aa61b1c930a7f79ff1aff366a378b16bbe2e4d48
Clément Contet <clement.contet@gmail.com>
2024-02-03 14:59:09 +0100

    It's 2024

--- app/src/main/java/com/example/tables/ComputeActivity.java
+++ app/src/main/java/com/example/tables/ComputeActivity.java
@@ -1,8 +1,8 @@
 package com.example.tables;
 
 import android.content.Intent;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
